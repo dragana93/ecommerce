a1922d42ab5bd6ac270b2b257c81710409858d13
Dragana Savanovic <dsavanovic@inimco.com>
2024-05-23 11:49:37 +0200

    API Generic Repository

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1488b8f06f51e057c00b123e78cafa55c38205e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbc89700b8c41198c7333b98915fb8f4b4502c82")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
